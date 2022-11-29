5657b4caa1502e696ab47ea388298be0b17f8817
drunkenhw <drunkenhw@gmail.com>
2022-11-29 13:48:06 +0900

    init project

---
+++ src/main/java/com/anonycar/AnonycarApplication.java
@@ -0,0 +1,13 @@
+package com.anonycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonycarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonycarApplication.class, args);
+	}
+
+}

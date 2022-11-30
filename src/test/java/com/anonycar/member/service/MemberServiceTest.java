32113fa22e82722737012d13e9cd32f2acdb31f3
drunkenhw <drunkenhw@gmail.com>
2022-12-01 00:11:33 +0900

    test: 로그인 기능 테스트 추가

--- src/test/java/com/anonycar/member/service/MemberServiceTest.java
+++ src/test/java/com/anonycar/member/service/MemberServiceTest.java
@@ -2,8 +2,13 @@ package com.anonycar.member.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import com.anonycar.member.domain.Member;
+import com.anonycar.member.domain.Password;
+import com.anonycar.member.dto.AuthMember;
 import com.anonycar.member.dto.JoinRequest;
+import com.anonycar.member.dto.LoginRequest;
 import com.anonycar.member.repository.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +24,34 @@ class MemberServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
-    @Test
+    @Autowired
+    private Encryptor encryptor;
+
+    @BeforeEach
+    void before() {
+        Password password = Password.of(encryptor, "Abcd123!@");
+        memberRepository.save(Member.builder()
+                .email("han@gmail.com")
+                .password(password)
+                .build());
+
+    }
+
     @DisplayName("회원가입을 한다")
+    @Test
     void joinTest() {
         String email = "email@gmail.com";
         JoinRequest joinRequest = new JoinRequest(email, "han", "Abc45678!", "drunken", "benz");
         memberService.join(joinRequest);
 
         assertThat(memberRepository.findByEmail(email)).isPresent();
+    }
+
+    @DisplayName("로그인 기능")
+    @Test
+    void login() {
+        AuthMember authInfo = memberService.login(new LoginRequest("han@gmail.com","Abcd123!@"));
+
+        assertThat(authInfo.getId()).isNotNull();
     }
 }
